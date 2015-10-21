f7a22876ef8785cea12c85ebaedd657f4571e28a
jackieontravel <jackieontravel@gmail.com>
2015-10-21 09:34:26 +0800

    fs utility: fs.sh and fs.py

---
+++ linux/fs.py
@@ -0,0 +1,272 @@
+#!/usr/bin/env python
+
+# This script works on Python 2.7
+import os
+import sys
+import cStringIO
+from subprocess import *
+
+
+#### User-specific variables, change to meet actual situations  ##############
+DISK_LETTER_NORMAL="U:"
+DISK_LETTER_MOCK="W:"
+EDITOR="notepad++"
+LN_NUM_FORMAT=" -n"
+FS_EXCLUDE_DIRS="-name .svn -o -name AppLibs -o -path ./BSEAV/bin -o -path ./out -o -name .git -o -name .repo -o -name objs"
+EXCLUDE_FILES="--exclude='*.d' --exclude='*.o' --exclude='*.so' --exclude='*.map' --exclude='ctags.tmp' --exclude='GPATH' --exclude='GRTAGS' --exclude='GTAGS' --exclude='tags'"
+
+# We don't skip too many dirs for ff, hopefully we can still find *.o
+FF_EXCLUDE_DIRS="-name .svn -o -name .git -o -name .repo"
+##############################################################################
+
+
+#### Common global variables, !!! DONT'T CHANGE !!! ##########################
+NPROC=2
+CMD_FILE=os.environ.get('HOME') + "/.fs_cmd_file"
+NOT_IN_MOCK=0
+sudo_cmd=""
+DISK_ROOT=""
+D_HOME=""
+DISK_LETTER=""
+##############################################################################
+
+# Check if we are in Mock. original Linux command:
+### export NOT_IN_MOCK=$(mount |grep chroot>/dev/null; echo $?)
+def checkIfRunningInMock():
+	global NOT_IN_MOCK, sudo_cmd, DISK_ROOT, D_HOME, DISK_LETTER
+
+	p1 = Popen(["mount"], stdout=PIPE)
+	p2 = Popen(["grep", "chroot"], stdin=p1.stdout, stdout=PIPE)
+	output = p2.communicate()[0]
+	if output:
+		NOT_IN_MOCK = 0
+		# setup other parameters under Mock
+		DISK_LETTER = DISK_LETTER_MOCK
+		sudo_cmd=""
+		if os.path.isfile("/builddir/.mock_name"):
+			f=open("/builddir/.mock_name")
+			MOCK_PREFIX=f.read().replace("\n", "")
+			f.close()
+			DISK_ROOT= "/" + MOCK_PREFIX + "/root"
+		else:
+			DISK_ROOT="Please get /builddir/.mock_name from external"
+		D_HOME=os.environ.get('HOME')
+	else:
+		NOT_IN_MOCK = 1
+		# setup other parameters in normal environment
+		DISK_LETTER = DISK_LETTER_NORMAL
+		sudo_cmd="sudo"
+		DISK_ROOT=""
+		D_HOME=""
+
+
+# Generate command file of fs which is then to be executed by shell.
+def genFsCmdFile(pattern, *options):
+	global NOT_IN_MOCK, sudo_cmd
+
+	print "fs v3.0 (20151016) - Find strings by Python pre-processing. Use 'fshelp' to check the usage.\n"
+
+	LINE_OUTPUT_FORMAT="\033[90m"   # Set to GRAY, see http://misc.flogisoft.com/bash/tip_colors_and_formatting for color details
+	LINE_OUTPUT_NORMAL="\033[0m"    # Restore to default
+
+	# collect all options as grep_opt:
+	# alwasy put "-e" as the last grep_opt as we'll put grep_opt right before grep pattern
+	grep_opt = ""
+	grep_regex = 0
+	for opt in options[0]:
+		if opt == "-e":
+			grep_regex = 1
+		else:
+			grep_opt += opt + " "
+	if grep_regex == 1:
+		grep_opt += "-e "
+	
+	# Get options from OS: fst(file_type), fsd(md) and fsopt(extra_opt)
+	fsType = os.environ.get('fst')
+	if fsType == "c":
+		# file_type="-regex '.*cp*$'"
+		file_type="-type f -name *.[c\|cc\|cpp]"
+	elif fsType == "h":
+		file_type="-type f -name *.[h\|hh]"
+	elif (fsType == "ch") | (fsType == "hc") :
+		file_type="-type f -name *.[c\|cc\|cpp\|h\|hh]"
+	elif fsType:
+		file_type="-type f -name " + '$fst'
+	else:
+		file_type="-type f"
+
+	fsDepth = os.environ.get('fsd')
+	if fsDepth:
+		md=int(fsDepth)
+	else:
+		md=9999
+		
+	# fsopt is used to specify find options.
+	if os.environ.get('fsopt'):
+		find_opt=os.environ.get('fsopt')
+	else:
+		find_opt=""
+
+	cmd_find = "time find " + "-maxdepth " + str(md) + " -type d \\( " + FS_EXCLUDE_DIRS + " \\) -prune -o " + file_type + " -print0" + find_opt
+	cmd_grep = "xargs -0 -P" + str(NPROC) + " grep -nIH " + EXCLUDE_FILES + " --color=always " + grep_opt + " '" + pattern + "'"
+
+	# The path under DISK_LETTER which we'll use in DOS 
+	path_under_disk = os.getcwd().replace(os.environ.get('HOME'), D_HOME)
+	path_under_disk = DISK_ROOT + path_under_disk
+	
+	awk_opt  = "-F':' "
+	awk_opt += ' -v prog="' + EDITOR + '"'
+	awk_opt += ' -v disk=' + DISK_LETTER 
+	awk_opt += ' -v root_path=' + path_under_disk
+	awk_opt += ' -v fmt=' + LINE_OUTPUT_FORMAT 
+	awk_opt += ' -v fmt_normal=' + LINE_OUTPUT_NORMAL 
+	awk_opt += ' -v lnfmt="' + LN_NUM_FORMAT + '"'
+	awk_opt += ' -v last_path='
+	
+	# Escape noteice: use '\\' to reprent '\' in awk programe
+	awk_line = []
+	awk_line.append( 'BEGIN {}' )
+	awk_line.append( '{' )
+	awk_line.append( '	path=root_path"/"$1;' )
+	awk_line.append( '	if ( path != last_path )' )
+	awk_line.append( '	{' )
+	awk_line.append( '		file_count++;' )
+	awk_line.append( '		last_path = path;' )
+	awk_line.append( '	}' )
+	awk_line.append( '	gsub("/", "\\\\", path);' )
+	awk_line.append( '	printf("%s%s %s%s%s%s%s\\n", fmt, prog, disk, path, lnfmt, $2, fmt_normal);' )
+	awk_line.append( '	printf("%s\\n", substr($0, 3+length($1)+length($2)));' )
+	awk_line.append( '}' )
+	awk_line.append( 'END {printf("\\n\\nTotal %d occurrences in %d files\\nRun again:\\nbash ' +  CMD_FILE + '\\n", NR, file_count)}' )
+	
+	# concatenate above awk script with new line ('\n') to be read easily
+	nl="\n"
+	awk_prog = nl.join(awk_line)
+	
+	cmd_awk = "awk " + awk_opt + " '" + awk_prog + "'"
+
+	f = open(CMD_FILE, 'w')
+	# Save current GREP_COLORS. see grep man page: http://linux.die.net/man/1/grep
+	f.write ('cur_grep_color=$GREP_COLORS\n')
+	# set GREP_COLORS: unset the colors of fn (filename), ln (line #), and se (separators) -- so that we can control the line output format by LINE_OUTPUT_FORMAT
+	f.write ('export GREP_COLORS="ms=01;31:mc=01;31:sl=:cx=:fn=:ln=:bn=32:se="\n')
+
+	f.write ( cmd_find + " | " + cmd_grep  + " | " + cmd_awk )
+	f.write ('\nexport GREP_COLORS=$cur_grep_color\n')
+
+	f.close()
+
+    # unset fsopt
+    # unset fsd
+    
+    # $sudo_cmd bash $CMD_FILE
+	
+
+
+def runFfCmd(pattern, *options):
+	# If we are in mock, sudo is not necessary.
+	if NOT_IN_MOCK == 1:
+		sudo_cmd="sudo"
+	else:
+		sudo_cmd=""
+
+	# Get options from OS: fft(ff_type)
+	convertToDos = 0
+	ffType = os.environ.get('fft')
+	if ffType == "ll":
+		post_op = "-exec ls -al {} \; "
+	elif ffType == "ls":
+		post_op = "-exec ls {} \; "
+	elif ffType == "rm":
+		post_op = "-exec /bin/rm -vrf {} \; "
+	else:
+		#default to use "dos" type
+		post_op="-print"
+		convertToDos = 1
+
+	# default set maxdepth to 9999
+	md=9999 
+    
+	# First check if maxdepth is assigned
+	if len(options[0]) > 0:
+		if options[0][0].isdigit():
+			md = int(options[0][0])
+			# delete maxdepth as we've consume it.
+			del options[0][0]
+    
+	# Then check if searching path is assigned
+	# NOTE: Put a 'smart' check on the path: first check if it's a relative path, otherwise treat it as a absolute path.
+	spath = "."
+	if len(options[0]) > 0:
+		if os.path.exists( "./" + options[0][0] ):
+			spath = "./" + options[0][0]
+		else:
+			spath = os.path.relpath( options[0][0], "." )
+
+    # check if pattern contains line number
+	if ":" in pattern:
+		filename=pattern.split(":")[0]
+		linenum=pattern.split(":")[1]
+	else:
+		filename=pattern
+		linenum=1
+	
+	cmd_find = sudo_cmd + ' find ' + spath + ' -maxdepth ' + str(md) + ' -type d \\( ' + FF_EXCLUDE_DIRS + ' \\) -prune -o ' +  '-name "' + filename + '" ' + post_op
+	
+	p1 = Popen(cmd_find, shell=True, stdout=PIPE)
+	output = p1.communicate()[0]
+	for line in output.split('\n'):
+		if line:
+			if convertToDos:
+				path = line
+				path = os.getcwd() + "/" + path
+				path = path.replace(os.environ.get('HOME'), D_HOME, 1)
+				path = DISK_ROOT + path
+				path = path.replace("/", '\\')
+				if linenum != 1:
+					print ( "%s %s%s %s%s" % (EDITOR, DISK_LETTER, path, LN_NUM_FORMAT, linenum))
+				else:
+					print ( "%s %s%s" % (EDITOR, DISK_LETTER, path))
+			else:
+				print line
+
+		
+def fs(argv):
+	options=""
+	
+	checkIfRunningInMock()
+
+	pattern = argv[0]
+	if len(argv) > 1 :
+		options = argv[1:]
+		
+	genFsCmdFile(pattern, options)
+
+	
+def ff(argv):
+	options=""
+	
+	checkIfRunningInMock()
+
+	pattern = argv[0]
+	if len(argv) > 1 :
+		options = argv[1:]
+		
+	runFfCmd(pattern, options)
+
+
+	
+	
+def main(argv):
+	if argv[1] == "fs":
+		fs (argv[2:])
+	elif argv[1] == "ff":
+		ff (argv[2:])
+	else:
+		print "Wrong usage! currently supported command: 'fs' and 'ff'"
+
+
+if __name__ == "__main__":
+	main(sys.argv)
+
+
---
+++ linux/fs.sh
@@ -0,0 +1,305 @@
+#!/bin/sh
+
+#############################################################################
+### Usage: 
+###	1. source fs.sh in .bashrc.
+### 2. Add PATH to where fs.py is located
+################################################################################
+function fs() 
+{ 
+	fs.py fs $*;
+	if [ $? == 0 ]; then 
+		source $HOME/.fs_cmd_file
+	fi
+}; 
+
+
+
+fshelp()
+{
+echo -e "**********"
+echo -e "fs"
+echo -e "~Jackie Yeh 2015/10/19 v3.0"
+echo -e "**********"
+echo -e "shell function to find string in all subdirectory, exclude:"
+echo -e " -- binary files (*.o; *.so; *.map; )"
+echo -e " -- all files version control: .svn, .git"
+echo -e " -- tagging system files:ctags, ctags.tmp, GPATH, GRTAGS, GTAGS, tags"
+echo -e " -- *.d"
+echo -e " "
+echo -e "Usage:"
+echo -e "     fs <String> [other grep options]"
+echo -e "NOTE:"
+echo -e "     wildcard pattern: in.*de will match inde, incde, inclde, include, ..."
+echo -e "Default options:"
+echo -e "     -n      print line number with output lines"
+echo -e "     -r      handle directories recursive"
+echo -e "     ---color=always      Always use colors on match"
+echo -e "Possible options:"
+echo -e "     -w      match only whole words"
+echo -e "     -i      ignore case distinctions"
+echo -e "     -e      use PATTERN as a regular expression"
+echo -e "     -l      only print FILE names containing matches"
+echo -e "     -NUM    print NUM lines of output context, NUM can be 1, 2, 3, ..."
+echo -e "     --include=PATTERN     files that match PATTERN will be examined"
+echo -e "     --exclude=PATTERN     files that match PATTERN will be skipped."
+echo -e "     --color=never         don't use color. Useful for file output"
+echo -e "Example:"
+echo -e " --Find 'layers_dbglist' in all subdirectories"
+echo -e "     fs layers_dbglist"
+echo -e " --Find 'layers_dbglist' and "match only whole words" in all subdirectories"
+echo -e "     fs layers_dbglist -w"
+echo -e " --Find 'layers_dbglist' and "ignore the case" in all subdirectories"
+echo -e "     fs layers_dbglist -i"
+echo -e " --Find 'layers_dbglist' and "print only filenames" in all subdirectories"
+echo -e "     fs layers_dbglist -l"
+echo -e " --Find 'layers_dbglist' and "show 3 lines context" in all subdirectories"
+echo -e "     fs layers_dbglist -3"
+echo -e " --Find 'layers_dbglist' in \"*.c;*.cpp;*.h\" under all subdirectories"
+echo -e "     fs layers_dbglist --include='*.c*' --include='*.h'"
+echo -e " --Find 'layers_dbglist' excluding \"*.h\" under all subdirectories"
+echo -e "     fs layers_dbglist --exclude='*.h'"
+echo -e " --Find 'not modal' under all subdirectories"
+echo -e "     fs 'not modal'"
+echo -e " "
+echo -e " Variants of fs:"
+echo -e "     fsu(): find in current directory, no recursion"
+echo -e "     fsc(): find pattern only in '*.c'"
+echo -e "     fsh(): find pattern only in '*.h'"
+echo -e "     fsd(): find <function declaration> or <structure definition> in *.c or *.cpp or *.h"
+echo -e "     fsds(): find <structure definition> in *.c or *.cpp or *.h"
+
+}
+
+
+# special version of fs(): find in cUrrent directory, no recursion
+fsu()
+{
+    fsd=1 fs $*
+}
+
+# special version of fs(): find pattern only in '*.c' or '*.cpp'
+fsc()
+{
+    fst=c fs $*
+}
+
+# special version of fs(): find pattern only in '*.h'
+fsh()
+{
+    fst=h fs $*
+}
+
+# special version of fs(): find function Declaration in *.c or *.cpp and *.h
+# How it works:
+#	"Normally" the following assumptions are made for the FUNCTION declarations:
+#		1. it exists in *.c/cpp or *.h
+#		2. There is no indent, and there are two possible conditions:
+#		2.1 : the first character is [a-zA-Z] and this line contains the function name
+#		2.2 : the line begins with function name and coming with others
+fsd()
+{
+    pattern=$1
+    option=
+    if [ "$#" -gt "1" ] ; then
+        shift
+        option=$*
+    fi
+
+    fst=ch fs "^[a-zA-Z].*$pattern\|^$pattern.*" -e $option
+}
+
+
+# special version of fsd(): find Structure declaration in *.c or *.cpp and *.h
+# How it works:
+#	"Normally" the following assumptions are made for the STRUCTURE declarations:
+#		1. it exists in *.c/cpp or *.h
+#		2. No indent, and the same line contains '}' and structure name
+#		3. The same line contains 'struct' and structure name
+# I said "normally" because it's not always the case, jsut an assumption...
+# NOTE: Difference between fsd and fsds:
+#	fsd: to find funciton declaration
+#	fsds: to find structure declaration
+fsds()
+{
+    pattern=$1
+    option=
+    if [ "$#" -gt "1" ] ; then
+        shift
+        option=$*
+    fi
+
+    fst=ch fs "^.*}.*$pattern\|struct.*$pattern" -e $option
+}
+
+
+
+
+################################################################################
+function ff() 
+{ 
+	fs.py ff $*;
+	# if [ $? == 0 ]; then 
+		# source $HOME/.ff_cmd_file
+	# fi
+}; 
+
+
+ffhelp()
+{
+	echo -e "**********"
+	echo -e "ff"
+	echo -e "~Jackie Yeh 2015/10/19 v3.0"
+	echo -e "**********"
+	echo -e "shell function to find files"
+	echo -e " "
+	echo -e "Usage can be illustrated by examples:"
+	echo -e "    ff example.c    -- Find the file: example.c"
+	echo -e "    ff \*.c         -- Find all *.c files"
+	echo -e "    ff \*.[ch]      -- Find all *.c or *.h files"
+	echo -e "    ff Makefile 1   -- Find 'Makefile' in current dir"
+	echo -e "    ff Makefile src -- Find 'Makefile' in 'src' dir"
+	echo -e "    ff .bashrc 1 ~  -- Find '.bashrc' in $HOME dir, don't search sub-dirs"
+	echo -e " Variants of ff:"
+	echo -e "     ffll(): find files, but don't convert to DOS format, instead show in ls long format"
+	echo -e "     ffls(): find files, but don't convert to DOS format, instead show in ls short format"
+	echo -e "     ffrm(): find files then remove it"
+
+}
+
+
+
+##varian: Find File then List in Long-format
+ffll()
+{
+    fft=ll ff  $*
+}
+
+
+
+##varian: Find File then List in single-line mode: so that I can do further shell operation
+ffls()
+{
+    fft=ls ff  $*
+}
+
+
+##varian: Find File and remove it
+ffrm()
+{
+    fft=rm ff  $*
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+############################################# BACK UP #################################
+# move fs to ofs() to temporarily back "old" fs()
+ofs()
+{
+    # If we are in mock, sudo is not necessary.
+    if [ "$NOT_IN_MOCK" -eq "1" ] ; then
+        sudo_cmd="sudo"                
+    else
+        sudo_cmd=""                
+    fi        
+    LINE_OUTPUT_FORMAT="\e[90m"   # Set to GRAY, see http://misc.flogisoft.com/bash/tip_colors_and_formatting for color details
+    LINE_OUTPUT_NORMAL="\e[0m"    # Restore to default
+    CMD_FILE=$HOME/.fs_cmd_file
+
+
+    pattern=$1
+    option=
+    if [ "$#" -gt "1" ] ; then
+        shift
+        option=$*
+    fi
+    
+    # fst: fs (t)ype
+    # Find only specific file types to descrease searching time.
+    if [ "$fst" == "" ] ; then
+        file_type="-type f"                
+    elif [ "$fst" == "c" ] ; then
+        file_type="-regex '.*cp*$'"
+    elif [ "$fst" == "h" ] ; then
+        file_type="-name '*.h'"
+    else
+        file_type="-name '$fst'"
+    fi
+    fst=""
+    
+
+    # fsd: fs (d)epth
+    # default set maxdepth to 9999
+    if [ "$fsd" == "" ] ; then
+        md=9999               
+    else
+        md=$fsd
+    fi
+
+    echo -e "fs v2.6 (20150904) - Find strings. Use 'fshelp' to check the usage.\n"
+    # Save current GREP_COLORS. see grep man page: http://linux.die.net/man/1/grep
+    echo -e "cur_grep_color=\"$GREP_COLORS\"" > $CMD_FILE
+    # set GREP_COLORS: unset the colors of fn (filename), ln (line #), and se (separators) -- so that we can control the line output format by LINE_OUTPUT_FORMAT
+    echo -e "export GREP_COLORS=\"ms=01;31:mc=01;31:sl=:cx=:fn=:ln=:bn=32:se=\"\n" >> $CMD_FILE
+    echo -e "time find $fsopt -maxdepth $md -type d \( -name '\.svn' -o -name 'AppLibs' -o -path './BSEAV/bin' \) -prune -o $file_type -print0 | xargs -0 grep -nIH --exclude='$CMD_FILE' --exclude='*.d' --exclude='*.o' --exclude='*.so' --exclude='*.map' --exclude='*.cmd' --exclude='ctags.tmp' --exclude='tags' --color=always $option '$pattern' \
+    | awk -F':' -v prog=\"$WINDOWS_PROGRAM\" -v disk=$DISK_LETTER -v root_path=\`pwd | sed \"s;$HOME;$D_HOME;\" | sed \"s;^;$DISK_ROOT;\"\` -v fmt=$LINE_OUTPUT_FORMAT -v fmt_normal=$LINE_OUTPUT_NORMAL -v lnfmt=\"$LN_NUM_FORMAT\" -v last_path="" '
+        BEGIN {}
+        {
+            path=root_path\"/\"\$1;
+            if ( path != last_path ) 
+			{
+                file_count++;
+                last_path = path;
+            }
+            gsub(\"/\", \"\\\\\\\", path);
+            printf(\"%s%s %s%s%s%s%s\\\n\", fmt, prog, disk, path, lnfmt, \$2, fmt_normal);
+            printf(\"%s\\\n\", substr(\$0, 3+length(\$1)+length(\$2)));
+        }
+        END {printf(\"\\\n\\\nTotal %d occurrences in %d files\\\nRun again:\\\nbash $CMD_FILE\\\n\", NR, file_count)}'" >> $CMD_FILE
+#        END {printf(\"\\\n\\\nTotal %d occurrences in %d files\\\nRun again:\\\nbash $CMD_FILE\\\n\", NR, file_count)}'" | tee $CMD_FILE -a
+    echo -e "( \"Regular expression\" style wildcard. Ex: fs 'in.*de' to find 'inde', 'include', 'inc abde',... )\n\n"
+
+    # restor GREP_COLORS
+    echo -e "export GREP_COLORS=\"$cur_grep_color\"" >> $CMD_FILE
+    
+    # restore control parameters
+    unset fsopt
+    unset fsd
+    
+    $sudo_cmd bash $CMD_FILE
+    #rm -rf .fs_cmd_file  #keep it to run again!
+
+}
+
