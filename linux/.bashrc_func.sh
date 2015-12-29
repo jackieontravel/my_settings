8c16559b22b61baafaaf8b6ac8f722d8d2267dc3
jackieontravel <jackieontravel@gmail.com>
2015-12-29 13:17:34 +0800

    [change] Simplify the definition of cgrep1 ~ cgrep6
    [add] Add a handy function: 'cgrep'
    
    [add] Add cgrep variants: cgrep1l ~ cgrep6l
    
    [add] Add ANSI color codes explanation and example: listcolor/listcolor1/listcolor2

--- linux/.bashrc_func.sh
+++ linux/.bashrc_func.sh
@@ -263,31 +263,352 @@ function rpmEx() { rpm2cpio $1 | cpio -idmv; }
 # Colorful grep: highlight the keywords and keep output others, especially useful in embedded system with full-function grep
 function cgrep1()
 {
-    GREP_COLORS="ms=01;31" grep --color=always -E "$1|\$"
+    GREP_COLORS="ms=01;31" grep --color=always -E "$1|$"
 }
 
 function cgrep2()
 {
-    GREP_COLORS="ms=01;32" grep --color=always -E "$1|\$"
+    GREP_COLORS="ms=01;32" grep --color=always -E "$1|$"
 }
 
 function cgrep3()
 {
-    GREP_COLORS="ms=01;33" grep --color=always -E "$1|\$"
+    GREP_COLORS="ms=01;33" grep --color=always -E "$1|$"
 }
 
 function cgrep4()
 {
-    GREP_COLORS="ms=01;34" grep --color=always -E "$1|\$"
+    GREP_COLORS="ms=01;34" grep --color=always -E "$1|$"
 }
 
 function cgrep5()
 {
-    GREP_COLORS="ms=01;35" grep --color=always -E "$1|\$"
+    GREP_COLORS="ms=01;35" grep --color=always -E "$1|$"
 }
 
 function cgrep6()
 {
-    GREP_COLORS="ms=01;36" grep --color=always -E "$1|\$"
+    GREP_COLORS="ms=01;36" grep --color=always -E "$1|$"
 }
 
+function cgrep()
+{
+    if [ $# -lt 1 ]; then
+        echo "Usage: cgrep [<pattern> [pattern2]...[pattern6]]"
+        return
+    fi
+    
+    cmd="cgrep1 $1"
+    shift
+    
+    if [ $# -ge 1 ]; then
+        cmd="$cmd | cgrep2 $1"
+        shift
+    fi
+    
+    if [ $# -ge 1 ]; then
+        cmd="$cmd | cgrep3 $1"
+        shift
+    fi
+    
+    if [ $# -ge 1 ]; then
+        cmd="$cmd | cgrep4 $1"
+        shift
+    fi
+    
+    if [ $# -ge 1 ]; then
+        cmd="$cmd | cgrep5 $1"
+        shift
+    fi
+    
+    if [ $# -ge 1 ]; then
+        cmd="$cmd | cgrep6 $1"
+        shift
+    fi
+    
+    eval $cmd
+}
+
+
+#colorful grep variants: highlight the whole line with background color: format cgrep<n>l. 'l' stands for line
+# TODO: Not a perfect solution yet, now highlight to the end of line, we need just the pattern itself
+# More possibility: http://stackoverflow.com/questions/17236005/grep-output-with-multiple-colors
+function cgrep1l()
+{
+    GREP_COLORS="ms=0;30;100" grep --color=always -E "^.*$1.*$|$" | cgrep1 "$1.*"
+}
+
+function cgrep2l()
+{
+    GREP_COLORS="ms=0;30;100" grep --color=always -E "^.*$1.*$|$" | cgrep2 "$1.*"
+}
+
+function cgrep3l()
+{
+    GREP_COLORS="ms=0;30;100" grep --color=always -E "^.*$1.*$|$" | cgrep3 "$1.*"
+}
+
+function cgrep4l()
+{
+    GREP_COLORS="ms=0;30;100" grep --color=always -E "^.*$1.*$|$" | cgrep4 "$1.*"
+}
+
+function cgrep5l()
+{
+    GREP_COLORS="ms=0;30;100" grep --color=always -E "^.*$1.*$|$" | cgrep5 "$1.*"
+}
+
+function cgrep6l()
+{
+    GREP_COLORS="ms=0;30;100" grep --color=always -E "^.*$1.*$|$" | cgrep6 "$1.*"
+}
+
+
+####################################################################################################
+# Explanation about ANSI color codes... used in GREP_COLORS
+#                   - summarized by Jackie on 2015/12/29, REF: http://linux-sxs.org/housekeeping/dircolor.html
+#
+# Below are the color init strings for the basic file types. A color init
+# string consists of one or more of the following numeric codes:
+# * Attribute codes:
+#       00=none 01=bold 04=underscore 05=blink 07=reverse 08=concealed
+# * Text color codes:
+#     - Normal intensity
+#       30=black 31=red 32=green 33=yellow 34=blue 35=magenta 36=cyan 37=white
+#     - High intensity
+#       90=black 91=red 92=green 93=yellow 94=blue 95=magenta 96=cyan 97=white
+# * Background color codes:
+#     - Normal intensity
+#       40=black 41=red 42=green 43=yellow 44=blue 45=magenta 46=cyan 47=white
+#     - High intensity
+#       100=black 101=red 102=green 103=yellow 104=blue 105=magenta 106=cyan 107=white
+#
+# Examples:
+#    NORMAL 00 # global default, although everything should be something.
+#    FILE 00 # normal file
+#    DIR 01;34 # directory
+#    LINK 01;36 # symbolic link
+#    FIFO 40;33 # pipe
+#    SOCK 01;35 # socket
+#    BLK 40;33;01 # block device driver
+#    CHR 40;33;01 # character device driver
+#    ORPHAN 40;31;01 # symlink to nonexistent file
+#
+
+####################################################################################################
+# List of colors for prompt and Bash. REF: https://wiki.archlinux.org/index.php/Color_Bash_Prompt
+txtblk='\e[0;30m' # Black - Regular
+txtred='\e[0;31m' # Red
+txtgrn='\e[0;32m' # Green
+txtylw='\e[0;33m' # Yellow
+txtblu='\e[0;34m' # Blue
+txtpur='\e[0;35m' # Purple
+txtcyn='\e[0;36m' # Cyan
+txtwht='\e[0;37m' # White
+bldblk='\e[1;30m' # Black - Bold
+bldred='\e[1;31m' # Red
+bldgrn='\e[1;32m' # Green
+bldylw='\e[1;33m' # Yellow
+bldblu='\e[1;34m' # Blue
+bldpur='\e[1;35m' # Purple
+bldcyn='\e[1;36m' # Cyan
+bldwht='\e[1;37m' # White
+undblk='\e[4;30m' # Black - Underline
+undred='\e[4;31m' # Red
+undgrn='\e[4;32m' # Green
+undylw='\e[4;33m' # Yellow
+undblu='\e[4;34m' # Blue
+undpur='\e[4;35m' # Purple
+undcyn='\e[4;36m' # Cyan
+undwht='\e[4;37m' # White
+bakblk='\e[40m'   # Black - Background
+bakred='\e[41m'   # Red
+bakgrn='\e[42m'   # Green
+bakylw='\e[43m'   # Yellow
+bakblu='\e[44m'   # Blue
+bakpur='\e[45m'   # Purple
+bakcyn='\e[46m'   # Cyan
+bakwht='\e[47m'   # White
+txtrst='\e[0m'    # Text Reset
+
+function listcolor1()
+{
+    echo txtblk=$txtblk
+    echo txtred=$txtred
+    echo txtgrn=$txtgrn
+    echo txtylw=$txtylw
+    echo txtblu=$txtblu
+    echo txtpur=$txtpur
+    echo txtcyn=$txtcyn
+    echo txtwht=$txtwht
+    echo bldblk=$bldblk
+    echo bldred=$bldred
+    echo bldgrn=$bldgrn
+    echo bldylw=$bldylw
+    echo bldblu=$bldblu
+    echo bldpur=$bldpur
+    echo bldcyn=$bldcyn
+    echo bldwht=$bldwht
+    echo undblk=$unkblk
+    echo undred=$undred
+    echo undgrn=$undgrn
+    echo undylw=$undylw
+    echo undblu=$undblu
+    echo undpur=$undpur
+    echo undcyn=$undcyn
+    echo undwht=$undwht
+    echo bakblk=$bakblk
+    echo bakred=$bakred
+    echo bakgrn=$bakgrn
+    echo bakylw=$bakylw
+    echo bakblu=$bakblu
+    echo bakpur=$bakpur
+    echo bakcyn=$bakcyn
+    echo bakwht=$bakwht
+    echo txtrst=$txtrst
+}
+
+
+
+####################################################################################################
+# More readable. again REF: https://wiki.archlinux.org/index.php/Color_Bash_Prompt
+# Reset
+Color_Off='\e[0m'       # Text Reset
+
+# Regular Colors
+Black='\e[0;30m'        # Black
+Red='\e[0;31m'          # Red
+Green='\e[0;32m'        # Green
+Yellow='\e[0;33m'       # Yellow
+Blue='\e[0;34m'         # Blue
+Purple='\e[0;35m'       # Purple
+Cyan='\e[0;36m'         # Cyan
+White='\e[0;37m'        # White
+
+# Bold
+BBlack='\e[1;30m'       # Black
+BRed='\e[1;31m'         # Red
+BGreen='\e[1;32m'       # Green
+BYellow='\e[1;33m'      # Yellow
+BBlue='\e[1;34m'        # Blue
+BPurple='\e[1;35m'      # Purple
+BCyan='\e[1;36m'        # Cyan
+BWhite='\e[1;37m'       # White
+
+# Underline
+UBlack='\e[4;30m'       # Black
+URed='\e[4;31m'         # Red
+UGreen='\e[4;32m'       # Green
+UYellow='\e[4;33m'      # Yellow
+UBlue='\e[4;34m'        # Blue
+UPurple='\e[4;35m'      # Purple
+UCyan='\e[4;36m'        # Cyan
+UWhite='\e[4;37m'       # White
+
+# Background
+On_Black='\e[40m'       # Black
+On_Red='\e[41m'         # Red
+On_Green='\e[42m'       # Green
+On_Yellow='\e[43m'      # Yellow
+On_Blue='\e[44m'        # Blue
+On_Purple='\e[45m'      # Purple
+On_Cyan='\e[46m'        # Cyan
+On_White='\e[47m'       # White
+
+# High Intensity
+IBlack='\e[0;90m'       # Black
+IRed='\e[0;91m'         # Red
+IGreen='\e[0;92m'       # Green
+IYellow='\e[0;93m'      # Yellow
+IBlue='\e[0;94m'        # Blue
+IPurple='\e[0;95m'      # Purple
+ICyan='\e[0;96m'        # Cyan
+IWhite='\e[0;97m'       # White
+
+# Bold High Intensity
+BIBlack='\e[1;90m'      # Black
+BIRed='\e[1;91m'        # Red
+BIGreen='\e[1;92m'      # Green
+BIYellow='\e[1;93m'     # Yellow
+BIBlue='\e[1;94m'       # Blue
+BIPurple='\e[1;95m'     # Purple
+BICyan='\e[1;96m'       # Cyan
+BIWhite='\e[1;97m'      # White
+
+# High Intensity backgrounds
+On_IBlack='\e[0;100m'   # Black
+On_IRed='\e[0;101m'     # Red
+On_IGreen='\e[0;102m'   # Green
+On_IYellow='\e[0;103m'  # Yellow
+On_IBlue='\e[0;104m'    # Blue
+On_IPurple='\e[0;105m'  # Purple
+On_ICyan='\e[0;106m'    # Cyan
+On_IWhite='\e[0;107m'   # White
+
+
+function listcolor2()
+{
+    echo Color_Off=$Color_Off
+    echo Black=$Black
+    echo Red=$Red
+    echo Green=$Green
+    echo Yellow=$Green
+    echo Blue=$Green
+    echo Purple=$Green
+    echo Cyan=$Green
+    echo White=$Green
+    echo BBlack=$Green
+    echo BRed=$Green
+    echo BGreen=$Green
+    echo BYellow=$Green
+    echo BBlue=$Green
+    echo BPurple=$Green
+    echo BCyan=$Green
+    echo BWhite=$Green
+    echo UBlack=$Green
+    echo URed=$Green
+    echo UGreen=$Green
+    echo UYellow=$Green
+    echo UBlue=$Green
+    echo UPurple=$Green
+    echo UCyan=$Green
+    echo UWhite=$Green
+    echo On_Black=$Green
+    echo On_Red=$Green
+    echo On_Green=$Green
+    echo On_Yellow=$Green
+    echo On_Blue=$Green
+    echo On_Purple=$Green
+    echo On_Cyan=$Green
+    echo On_White=$Green
+    echo IBlack=$Green
+    echo IRed=$Green
+    echo IGreen=$Green
+    echo IYellow=$Green
+    echo IBlue=$Green
+    echo IPurple=$Green
+    echo ICyan=$Green
+    echo IWhite=$Green
+    echo BIBlack=$Green
+    echo BIRed=$Green
+    echo BIGreen=$Green
+    echo BIYellow=$Green
+    echo BIBlue=$Green
+    echo BIPurple=$Green
+    echo BICyan=$Green
+    echo BIWhite=$Green
+    echo On_IBlack=$Green
+    echo On_IRed=$Green
+    echo On_IGreen=$Green
+    echo On_IYellow=$Green
+    echo On_IBlue=$Green
+    echo On_IPurple=$Green
+    echo On_ICyan=$Green
+    echo On_IWhite=$Green
+}
+
+alias listcolor='listcolor1; listcolor2'
+####################################################################################################
+# Usage:
+#   Show color code:    echo $Red
+#   Show color effect:  echo -e "$Red text"
